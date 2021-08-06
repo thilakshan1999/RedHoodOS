3ff4e9526d1fd00440223a11e0c73f4a216c2909
Red Hood <62758741+thilakshan1999@users.noreply.github.com>
2021-08-06 20:11:56 +0530

    Add files via upload

--- kmain.c
+++ kmain.c
@@ -1,4 +1,14 @@
-    int sum_of_three(int arg1, int arg2, int arg3)
-    {
-        return arg1 + arg2 + arg3;
-    }
+#include "drivers/frame_buffer.h"
+#include "drivers/serial_port.h"
+
+/* The C function */
+int sum_of_three(int arg1, int arg2, int arg3)
+{
+char RH[] = "Red Hood ";
+
+fb_write(RH, 19);
+serial_write(0x3F8, RH, 19);
+
+return arg1 + arg2 + arg3;
+}
+
