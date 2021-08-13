c50f0dce7f6753b1de45cb3630a3152ee15659a7
Red Hood <62758741+thilakshan1999@users.noreply.github.com>
2021-08-13 20:03:03 +0530

    Add files via upload (#4)

--- kmain.c
+++ kmain.c
@@ -1,9 +1,10 @@
 #include "drivers/frame_buffer.h"
 #include "drivers/serial_port.h"
-
+#include "segmentation/memory_segments.h"
 /* The C function */
 int sum_of_three(int arg1, int arg2, int arg3)
 {
+segments_install_gdt();
 char RH[] = "Red Hood ";
 
 fb_write(RH, 19);
