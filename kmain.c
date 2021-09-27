cb3f4fd63fff74bad791ab0e221521636703cb39
Red Hood <62758741+thilakshan1999@users.noreply.github.com>
2021-09-27 23:01:46 +0530

    Add files via upload

--- kmain.c
+++ kmain.c
@@ -3,34 +3,22 @@
 #include "drivers/interrupts/interrupts.h"
 #include "multiboot.h"
 #include "memory/paging/paging.h"
+#include "user_mode/user_mode.h"
+#include "drivers/interrupts/interrupt_enabler.h"
 
-void init(){
+void init(u32int kernelPhysicalStart, u32int kernelPhysicalEnd){
    segments_install_gdt();
    interrupts_install_idt();
-   init_paging();
+   init_paging(kernelPhysicalStart, kernelPhysicalEnd);
 }
 
-int kmain(unsigned int ebx){
+int kmain(unsigned int ebx, u32int kernel_physical_start, u32int kernel_physical_end){
    
-   init();
+   init(kernel_physical_start, kernel_physical_end);
 
-   multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
-   multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
-   unsigned int address_of_module = modules->mod_start;
-
-   if((mbinfo->mods_count) == 1){
-      char str[] = "Module successfully loaded\n";
-      serial_write(str,sizeof(str));
-      
-      typedef void (*call_module_t)(void);
-         call_module_t start_program = (call_module_t) address_of_module;
-         start_program();
-
-   }
-   else{
-      char str[] = "Multiple modules loaded\n";
-      serial_write(str,sizeof(str));
-   }
+   disable_hardware_interrupts();
+  	
+  	switch_to_user_mode();
 
    return 0;
 }
